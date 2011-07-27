b52715942aa3af5ff0d6805669a775796c63e6a6
Shinya Kuribayashi <shinya.kuribayashi.px@renesas.com>
2011-07-27 16:51:42 +0900

    sh: clkfwk: CKSEL clock support
    Redmine: refs #20929
    
    In some DIV6 xxxCKCR registers in R-Mobile APE5R, FSI[AB]CKCR, SUBCKCR
    and SPU[AV]CKCR, we have CKSEL bit aside from EXSRC bit.  CKSEL bit is
    distinuished from EXSRC bit by the fact that it selects clock source of
    DIV6 _output_ clock, not select the clock source of the _divider_.
    
    If xxCKCR.CKSEL=1, an external clock source is slected as output clock,
    and DIV6 divider is _not_used_.  This means that we can not apply
    existing DIV6 clock infrastructure for such DIV6 clocks.
    
    This patch introduces CKSEL clock variant based on DIV6 clock support,
    specialized for 'set_parent' function.  There's no frequency table, no
    div_mult_table, no clock rate operations; just follows parent clock
    frequency.
    
    Note: enable/disable operations are supported, but might be redundant.
    There're various hardware / use cases, and I couldn't make a decision
    right now.
    
    Signed-off-by: Shinya Kuribayashi <shinya.kuribayashi.px@renesas.com>

--- include/linux/sh_clk.h
+++ include/linux/sh_clk.h
@@ -60,6 +60,7 @@ struct clk {
 
 #define CLK_ENABLE_ON_INIT	(1 << 0)
 #define CLK_DIV_SHARED		(1 << 1)
+#define CLK_CKSEL_CKSTP		(1 << 2)
 
 /* drivers/sh/clk.c */
 unsigned long followparent_recalc(struct clk *);
@@ -160,5 +161,20 @@ int sh_clk_div6_reparent_register(struct clk *clks, int nr);
 #define CLKDEV_CON_ID(_id, _clk) { .con_id = _id, .clk = _clk }
 #define CLKDEV_DEV_ID(_id, _clk) { .dev_id = _id, .clk = _clk }
 #define CLKDEV_ICK_ID(_cid, _did, _clk) { .con_id = _cid, .dev_id = _did, .clk = _clk }
+
+#define SH_CLK_CKSEL(_parent, _reg, _bit, _flags, _parents,	\
+		     _num_parents, _src_shift, _src_width)	\
+{								\
+	.parent = _parent,					\
+	.enable_reg = (void __iomem *)_reg,			\
+	.enable_bit = _bit,					\
+	.flags = _flags,					\
+	.parent_table = _parents,				\
+	.parent_num = _num_parents,				\
+	.src_shift = _src_shift,				\
+	.src_width = _src_width,				\
+}
+
+int sh_clk_cksel_register(struct clk *clks, int nr);
 
 #endif /* __SH_CLOCK_H */
