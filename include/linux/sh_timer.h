be94b7dba89b2d236c947e504204db96e1225156
Shinya Kuribayashi <shinya.kuribayashi.px@renesas.com>
2011-08-30 17:17:49 +0900

    clocksource: sh_cmt: Make CMCSR.CKS[2:0] selectable by user
    Redmine: refs #22509
    
    This patch doesn't try to maintain backward compatibility with existing
    sh_cmt driver and its platform data interface.  All users need to
    provide cks_table[], cks_num and cks in accordance with their platforms.
    
    Prototyped-by: Teppei Kamijou <teppei.kamijou.yb@renesas.com>
    [ skuribay: adapted to the latest version (v3.2) of sh_cmt driver ]
    Signed-off-by: Shinya Kuribayashi <shinya.kuribayashi.px@renesas.com>

--- include/linux/sh_timer.h
+++ include/linux/sh_timer.h
@@ -1,12 +1,21 @@
 #ifndef __SH_TIMER_H__
 #define __SH_TIMER_H__
 
+struct sh_timer_clock {
+	const char *name;
+	unsigned int divisor;
+};
+
 struct sh_timer_config {
 	char *name;
 	long channel_offset;
 	int timer_bit;
 	unsigned long clockevent_rating;
 	unsigned long clocksource_rating;
+
+	struct sh_timer_clock *cks_table;
+	unsigned short cks_num;
+	unsigned short cks;
 };
 
 #endif /* __SH_TIMER_H__ */
