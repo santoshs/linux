a38394490a1ad3d2999d4761f39b5526c35345a6
Teppei Kamijou <teppei.kamijou.yb@renesas.com>
2011-10-04 14:41:35 +0900

    mmc: renesas_mmcif: Fix ->set_pwr() / ->down_pwr() usage bug
    Redmine: refs #26860 (Summary #26669)
    
    set_pwr()/down_pwer() should be maintained in accordance with
    host->card_present changes, not host->power.
    
    Signed-off-by: Teppei Kamijou <teppei.kamijou.yb@renesas.com>
    Signed-off-by: Shinya Kuribayashi <shinya.kuribayashi.px@renesas.com>

--- drivers/mmc/host/renesas_mmcif.c
+++ drivers/mmc/host/renesas_mmcif.c
@@ -898,6 +898,8 @@ static void sh_mmcif_set_ios(struct mmc_host *mmc, struct mmc_ios *ios)
 			/* See if we also get DMA */
 			sh_mmcif_request_dma(host, host->pd->dev.platform_data);
 			host->card_present = true;
+			if (p->set_pwr)
+				p->set_pwr(host->pd, ios->power_mode);
 		}
 	} else if (ios->power_mode == MMC_POWER_OFF || !ios->clock) {
 		/* clock stop */
@@ -906,13 +908,13 @@ static void sh_mmcif_set_ios(struct mmc_host *mmc, struct mmc_ios *ios)
 			if (host->card_present) {
 				sh_mmcif_release_dma(host);
 				host->card_present = false;
+				if (p->down_pwr)
+					p->down_pwr(host->pd);
 			}
 		}
 		if (host->power) {
 			pm_runtime_put(&host->pd->dev);
 			host->power = false;
-			if (p->down_pwr && ios->power_mode == MMC_POWER_OFF)
-				p->down_pwr(host->pd);
 		}
 		host->state = STATE_IDLE;
 		return;
@@ -920,8 +922,6 @@ static void sh_mmcif_set_ios(struct mmc_host *mmc, struct mmc_ios *ios)
 
 	if (ios->clock) {
 		if (!host->power) {
-			if (p->set_pwr)
-				p->set_pwr(host->pd, ios->power_mode);
 			pm_runtime_get_sync(&host->pd->dev);
 			host->power = true;
 			sh_mmcif_sync_reset(host);
