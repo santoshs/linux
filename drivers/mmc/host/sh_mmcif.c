fa6fbc10376e2c15f36dc3ce8bdecb5bb5949945
Teppei Kamijou <teppei.kamijou.yb@renesas.com>
2011-12-27 16:31:39 +0900

    mmc: sh_mmcif: Make 'maximum clock rate' user-configurable
    Redmine: refs #26865
    
    Signed-off-by: Teppei Kamijou <teppei.kamijou.yb@renesas.com>
    Signed-off-by: Shinya Kuribayashi <shinya.kuribayashi.px@renesas.com>

--- drivers/mmc/host/sh_mmcif.c
+++ drivers/mmc/host/sh_mmcif.c
@@ -1099,7 +1099,10 @@ static int __devinit sh_mmcif_probe(struct platform_device *pdev)
 	spin_lock_init(&host->lock);
 
 	mmc->ops = &sh_mmcif_ops;
-	mmc->f_max = host->clk / 2;
+	if (pd->max_clk)
+		mmc->f_max = pd->max_clk;
+	else
+		mmc->f_max = host->clk / 2;
 	/* close to 400KHz */
 	if (host->clk < 51200000)
 		mmc->f_min = host->clk / 128;
