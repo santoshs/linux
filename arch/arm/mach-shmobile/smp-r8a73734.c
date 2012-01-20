3e37e2768d3db962ed5881c0a09d7b7a4a8242e1
Teppei Kamijou <teppei.kamijou.yb@renesas.com>
2012-01-20 23:51:31 +0900

    smp-r8a73734: R-Mobile U2 ES1.0 erratum (ES1 ECR0217) workaround
    1. Store __pa(shmobile_secondary_vector) into the SBAR2 register
    2. Set _zero_ address into the SBAR register
       - When CPU#1 is waken up, it gets started from 0x0
    3. U-Boot restores __pa(shmobile_secondary_vector) from the SBAR2, and
       get CPU#1 redirected to the physical address.
    
    Signed-off-by: Teppei Kamijou <teppei.kamijou.yb@renesas.com>
    Signed-off-by: Shinya Kuribayashi <shinya.kuribayashi.px@renesas.com>

--- arch/arm/mach-shmobile/smp-r8a73734.c
+++ arch/arm/mach-shmobile/smp-r8a73734.c
@@ -31,6 +31,7 @@
 #define SRESCR		0xe6151018
 #define PSTR		0xe6151040
 #define SBAR            0xe6180020
+#define SBAR2           0xe6180060
 #define APARMBAREA      0xe6f10020
 
 static void __iomem *scu_base_addr(void)
@@ -91,7 +92,12 @@ void __init r8a73734_smp_prepare_cpus(void)
 
 	/* Map the reset vector (in headsmp.S) */
 	__raw_writel(0, __io(APARMBAREA));      /* 4k */
+#if 0
 	__raw_writel(__pa(shmobile_secondary_vector), __io(SBAR));
+#else
+	__raw_writel(__pa(shmobile_secondary_vector), __io(SBAR2));
+	__raw_writel(0, __io(SBAR));
+#endif
 
 	/* enable cache coherency on CPU0 */
 	modify_scu_cpu_psr(0, 3 << (0 * 8));
