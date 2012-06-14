9cb47be2424fcdadee8ecdd11a1a97ca23019b3e
Shinya Kuribayashi <shinya.kuribayashi.px@renesas.com>
2012-06-14 18:03:58 +0900

    hwspinlock/core: use global ID to register hwspinlocks on multiple devices (amend)
    Redmine: N/A
    
    Commit 300bab9770 (hwspinlock/core: register a bank of hwspinlocks in a
    single API call, 2011-09-06) introduced 'hwspin_lock_register_single()'
    to register numerous (a bank of) hwspinlock instances in a single API,
    'hwspin_lock_register()'.
    
    At which time, 'hwspin_lock_register()' accidentally passes 'local IDs'
    to 'hwspin_lock_register_single()', despite that ..._single() requires
    'global IDs' to register hwspinlocks.
    
    We have to convert into global IDs by supplying the missing 'base_id'.
    
    Reported-by: Yasuo Matsushiro <yasuo.matsushiro.ym@bp.renesas.com>
    Signed-off-by: Shinya Kuribayashi <shinya.kuribayashi.px@renesas.com>

--- drivers/hwspinlock/hwspinlock_core.c
+++ drivers/hwspinlock/hwspinlock_core.c
@@ -354,7 +354,7 @@ int hwspin_lock_register(struct hwspinlock_device *bank, struct device *dev,
 
 reg_failed:
 	while (--i >= 0)
-		hwspin_lock_unregister_single(i);
+		hwspin_lock_unregister_single(base_id + i);
 	return ret;
 }
 EXPORT_SYMBOL_GPL(hwspin_lock_register);
