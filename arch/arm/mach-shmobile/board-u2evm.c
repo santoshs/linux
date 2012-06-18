5756a026e068c3253c2a589d49edc2e00726a306
BCAP <vinay.hv@borqs.com>
2012-06-18 17:55:27 +0530

    Revert "Modem reset v1" additional revert to fix build issue

--- arch/arm/mach-shmobile/board-u2evm.c
+++ arch/arm/mach-shmobile/board-u2evm.c
@@ -1623,7 +1623,6 @@ static struct platform_device *u2evm_devices_stm_none[] __initdata = {
 	&mfis_device,
 	&mipidsi0_device,
 	&tpu_devices[TPU_MODULE_0],
-	&mdm_reset_device,
 	&pcm2pwm_device,
 #ifdef CONFIG_SPI_SH_MSIOF
 	&sh_msiof0_device,
@@ -1674,7 +1673,6 @@ static struct platform_device *u2evm_devices_stm_sdhi1_no_cws[] __initdata = {
 	&lcdc_device,
 	&mipidsi0_device,
 	&tpu_devices[TPU_MODULE_0],
-	&mdm_reset_device,
 	&pcm2pwm_device,
 #ifdef CONFIG_SPI_SH_MSIOF
 	&sh_msiof0_device,
@@ -1764,7 +1762,6 @@ static struct platform_device *u2evm_devices_stm_none_no_cws[] __initdata = {
 	&mfis_device,
 	&mipidsi0_device,
 	&tpu_devices[TPU_MODULE_0],
-	&mdm_reset_device,
 	&pcm2pwm_device,
 #ifdef CONFIG_SPI_SH_MSIOF
 	&sh_msiof0_device,
