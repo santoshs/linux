8878476765e9022496a58d3f5d344e3f960ef555
Jerome Huchet <jerome.huchet@renesasmobile.com>
2012-06-22 09:52:57 +0200

    add missing mdm_reset_device to stm=0

--- arch/arm/mach-shmobile/board-u2evm.c
+++ arch/arm/mach-shmobile/board-u2evm.c
@@ -1455,6 +1455,7 @@ static struct platform_device *u2evm_devices_stm_sdhi0[] __initdata = {
 	&mfis_device,
 	&mipidsi0_device,
 //	&tpu_devices[TPU_MODULE_0],
+	&mdm_reset_device,
 //	&pcm2pwm_device,
 #ifdef CONFIG_SPI_SH_MSIOF
 	&sh_msiof0_device,
