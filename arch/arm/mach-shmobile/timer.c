b17c9afc69fa576a96464410dc82e9b2e56bb614
Shinya Kuribayashi <shinya.kuribayashi.px@renesas.com>
2012-07-06 19:40:27 +0900

    ARM: shmobile: Yet another CMT driver for R-Mobile devices
    Redmine: N/A (see also #22613)
    
    We need a dedicated clockevent device per Cortex-A9 CPU core that runs
    at a constant rate, in the face of core frequency scaling.
    
    We already have a driver for CMT module at drivers/clocksource/sh_cmt.c,
    it cannot be usable for the following reasons:
    
    * it's a platform driver, and hard to be used for local timers
    
    * it's complicated; clocksource and clockevent are handled in a single
      driver and they are processed concurrently, as a result its counting
      operation is inaccurate and unreliable
    
    * standard 16-bit based CMT is supported, but new one (32-bit variant
      introduced in R-Mobile U2) is not supported
    
    This is the first step in order to solve these issues.
    
    Signed-off-by: Shinya Kuribayashi <shinya.kuribayashi.px@renesas.com>

--- arch/arm/mach-shmobile/timer.c
+++ arch/arm/mach-shmobile/timer.c
@@ -21,26 +21,12 @@
 #include <linux/clk.h>
 #include <linux/delay.h>
 #include <linux/err.h>
-#include <linux/platform_device.h>
 #include <asm/mach/time.h>
-#include <asm/smp_twd.h>
 
 static void __init shmobile_late_time_init(void)
 {
 	struct clk *clkp;
 	unsigned long lpj;
-
-	/*
-	 * Make sure all compiled-in early timers register themselves.
-	 *
-	 * Run probe() for two "earlytimer" devices, these will be the
-	 * clockevents and clocksource devices respectively. In the event
-	 * that only a clockevents device is available, we -ENODEV on the
-	 * clocksource and the jiffies clocksource is used transparently
-	 * instead. No error handling is necessary here.
-	 */
-	early_platform_driver_register_all("earlytimer");
-	early_platform_driver_probe("earlytimer", 2, 0);
 
 	if (lpj_fine)
 		return; /* seems to be set up for the current timer */
