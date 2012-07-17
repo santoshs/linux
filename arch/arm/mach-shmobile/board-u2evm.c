ea7f791de16bcd0c134c55f55cde1ee7f212a0bb
tainguyen <tai.nguyen.pd@rvc.renesas.com>
2012-07-17 03:25:36 -0700

    Update USB OTG driver to apply HPB semaphore

--- arch/arm/mach-shmobile/board-u2evm.c
+++ arch/arm/mach-shmobile/board-u2evm.c
@@ -58,6 +58,10 @@
 
 #include <linux/mmcoops.h>
 
+#ifdef CONFIG_USB_OTG
+#include <linux/usb/tusb1211.h>
+#endif
+
 #define CLASHLOG_R_LOCAL_VER_LOCATE		0x4C801000
 #define CLASHLOG_R_LOCAL_VER_LENGTH       	32
 
@@ -384,6 +388,10 @@ static struct platform_device usb_host_device = {
 #endif /*CONFIG_USB_R8A66597_HCD*/
 #ifdef CONFIG_USB_OTG
 /*TUSB1211 OTG*/
+static struct tusb1211_platform_data tusb1211_data = {
+	.module_start = usbhs_module_reset,
+};
+
 static struct resource tusb1211_resource[] = {
 	[0] = {
 		.name	= "tusb1211_resource",
@@ -401,6 +409,9 @@ static struct resource tusb1211_resource[] = {
 static struct platform_device tusb1211_device = {
 	.name = "tusb1211_driver",
 	.id = 0,
+	.dev = {
+		.platform_data = &tusb1211_data,
+	},
 	.num_resources = ARRAY_SIZE (tusb1211_resource),
 	.resource = tusb1211_resource,
 };
