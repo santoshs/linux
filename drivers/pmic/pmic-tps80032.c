db963d1aa6ba1e39b5dab30fa0ef4102a5642209
chanvu <chan.vu.xv@rvc.renesas.com>
2012-11-06 03:57:01 -0800

    Change hwspin_lock ID

--- drivers/pmic/pmic-tps80032.c
+++ drivers/pmic/pmic-tps80032.c
@@ -1227,7 +1227,7 @@ static void tps80032_interrupt_work(void)
 			key_count = 1;
 			input_event(button_dev, EV_KEY, KEY_POWER, key_count);
 			input_sync(button_dev);
-            printk("Onkey pressed...\n");
+			PMIC_DEBUG_MSG("Onkey pressed...\n");
 		}
 	}
 
@@ -1235,7 +1235,8 @@ static void tps80032_interrupt_work(void)
 		key_count = (key_count + 1) % 2;
 		input_event(button_dev, EV_KEY, KEY_POWER, key_count);
 		input_sync(button_dev);
-        printk("Onkey %s...\n", key_count == 0 ? "released" : "pressed" );
+		PMIC_DEBUG_MSG("Onkey %s...\n",
+			key_count == 0 ? "released" : "pressed");
 	}
 
 	if (0 != (MSK_BIT_4 & sts_c)) {
@@ -4292,7 +4293,8 @@ static void tps80032_bat_update(struct tps80032_data *data)
 
 
 /*
- * tps80032_ext_work: define the external device which is inserted and notify the presence of external device
+ * tps80032_ext_work: define the external device which is
+ * inserted and notify the presence of external device
  * return: void
  */
 static void tps80032_ext_work(void)
@@ -4343,7 +4345,8 @@ static void tps80032_update_work(struct work_struct *work)
 }
 
 /*
- * tps80032_int_chrg_work: update all battery information and notify the change of battery state
+ * tps80032_int_chrg_work: update all battery information
+ * and notify the change of battery state
  * return: void
  */
 static void tps80032_int_chrg_work(void)
@@ -4438,7 +4441,8 @@ static void tps80032_int_chrg_work(void)
 
 
 /*
- * tps80032_chrg_ctrl_work: update all battery information and notify the change of battery state
+ * tps80032_chrg_ctrl_work: update all battery information
+ * and notify the change of battery state
  * return: void
  */
 static void tps80032_chrg_ctrl_work(void)
@@ -4481,7 +4485,8 @@ static void tps80032_chrg_ctrl_work(void)
 }
 
 /*
- * tps80032_vac_charger_work: detect VAC charger and set current limit when VAC charger is plugged
+ * tps80032_vac_charger_work: detect VAC charger and set
+ * current limit when VAC charger is plugged
  * return: void
  */
 static void tps80032_vac_charger_work(void)
@@ -5397,7 +5402,7 @@ static int tps80032_get_temp_status(struct device *dev)
 		ret = data->bat_temp;
 	else
 		ret = 0;
-	
+
 
 	PMIC_DEBUG_MSG("%s end <<<\n", __func__);
 	return ret;
@@ -6903,7 +6908,7 @@ static int __init tps80032_power_init(void)
 	PMIC_DEBUG_MSG(">>> %s start\n", __func__);
 
 	/*Initialize hw spinlock*/
-	r8a73734_hwlock_pmic = hwspin_lock_request_specific(SMGP000);
+	r8a73734_hwlock_pmic = hwspin_lock_request_specific(SMGP000_PMIC);
 	if (r8a73734_hwlock_pmic == NULL) {
 		PMIC_ERROR_MSG(
 		"Unable to register hw spinlock for pmic driver\n");
