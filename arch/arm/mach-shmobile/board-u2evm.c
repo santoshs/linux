84f0a0e6be12bf19a052f428705842e35c19dc72
chanvu <chan.vu.xv@rvc.renesas.com>
2012-11-13 02:12:19 -0800

    EOS2_SSG_JB30_RVC_PMIC_AY12110526612_Separate_PMIC_source_GED_rebase_20121113 changes merged

--- arch/arm/mach-shmobile/board-u2evm.c
+++ arch/arm/mach-shmobile/board-u2evm.c
@@ -26,8 +26,11 @@
 #include <linux/pmic/pmic-tps80032.h>
 #endif
 #include <linux/mfd/tps80031.h>
-#include <linux/spi/sh_msiof.h>
+#include <linux/pmic/pmic.h>
+#include <mach/setup-u2tps80032.h>
 #include <linux/regulator/tps80031-regulator.h>
+#include <linux/spi/sh_msiof.h>
+#include <linux/i2c/atmel_mxt_ts.h>
 #include <linux/usb/r8a66597.h>
 #include <linux/ion.h>
 #include <linux/memblock.h>
@@ -126,12 +129,15 @@ enum {
 #endif
 
 
+
 #if defined(CONFIG_MPU_SENSORS_MPU6050B1) || \
 	defined(CONFIG_OPTICAL_TAOS_TRITON) || \
 	defined(CONFIG_OPTICAL_GP2AP020A00F) || \
 	defined(CONFIG_INPUT_YAS_SENSORS)
 static void sensor_power_on_vdd(int);
 #endif
+
+
 
 
 
@@ -213,6 +219,7 @@ static int u2_read_board_rev(char *page, char **start, off_t off,
 	count = snprintf(page, count, "%x", u2_board_rev);
 	return count;
 }
+
 
 static struct resource smsc9220_resources[] = {
 	{
@@ -2450,103 +2457,6 @@ static struct platform_device *u2evm_devices_stm_none[] __initdata = {
 };
 
 /* I2C */
-
-static struct regulator_consumer_supply tps80031_ldo5_supply[] = {
-	REGULATOR_SUPPLY("vdd_touch", NULL),
-};
-
-#define TPS_PDATA_INIT(_id, _minmv, _maxmv, _supply_reg, _always_on,	\
-	_boot_on, _apply_uv, _init_uV, _init_enable, _init_apply, 	\
-	_flags, _delay)						\
-	static struct tps80031_regulator_platform_data pdata_##_id = {	\
-		.regulator = {						\
-			.constraints = {				\
-				.min_uV = (_minmv)*1000,		\
-				.max_uV = (_maxmv)*1000,		\
-				.valid_modes_mask = (REGULATOR_MODE_NORMAL |  \
-						REGULATOR_MODE_STANDBY),      \
-				.valid_ops_mask = (REGULATOR_CHANGE_MODE |    \
-						REGULATOR_CHANGE_STATUS |     \
-						REGULATOR_CHANGE_VOLTAGE),    \
-				.always_on = _always_on,		\
-				.boot_on = _boot_on,			\
-				.apply_uV = _apply_uv,			\
-			},						\
-			.num_consumer_supplies =			\
-				ARRAY_SIZE(tps80031_##_id##_supply),	\
-			.consumer_supplies = tps80031_##_id##_supply,	\
-			.supply_regulator = _supply_reg,		\
-		},							\
-		.init_uV =  _init_uV * 1000,				\
-		.init_enable = _init_enable,				\
-		.init_apply = _init_apply,				\
-		.flags = _flags,					\
-		.delay_us = _delay,					\
-	}
-
-TPS_PDATA_INIT(ldo5, 1000, 3300, 0, 0, 0, 0, 2700, 0, 1, 0, 0);
-
-static struct tps80031_rtc_platform_data rtc_data = {
-	.irq = ENT_TPS80031_IRQ_BASE + TPS80031_INT_RTC_ALARM,
-	.time = {
-		.tm_year = 2012,
-		.tm_mon = 0,
-		.tm_mday = 1,
-		.tm_hour = 1,
-		.tm_min = 2,
-		.tm_sec = 3,
-	},
-};
-
-#define TPS_REG(_id, _data)				\
-	{						\
-		.id	 = TPS80031_ID_##_id,		\
-		.name   = "tps80031-regulator",		\
-		.platform_data  = &pdata_##_data,	\
-	}
-
-#define TPS_RTC()				\
-	{					\
-		.id	= 0,			\
-		.name	= "rtc_tps80032",	\
-		.platform_data = &rtc_data,	\
-	}
-
-static struct tps80032_subdev_info tps80032_devs[] = {
-	TPS_RTC(),
-	TPS_REG(LDO5, ldo5),
-};
-#define PORTCR0			IO_ADDRESS(0xE6050000)
-#define PORTCR28		IO_ADDRESS(0xE605001C)
-#define PORTCR35		IO_ADDRESS(0xE6050023)
-#define PORTCR141		IO_ADDRESS(0xE605108D)
-#define PORTCR202		IO_ADDRESS(0xE60520CA)
-
-static u8 tps80032_get_portcr_value(u32 addr)
-{
-	return __raw_readb(addr);
-}
-
-static void tps80032_set_portcr_value(u8 value, u32 addr)
-{
-	__raw_writeb(value, addr);
-}
-static struct tps80032_platform_data tps_platform = {
-	.pin_gpio	= {GPIO_PORT0, GPIO_PORT28,
-				GPIO_PORT35, GPIO_PORT141,
-				GPIO_PORT202},
-	.pin_gpio_fn	= {GPIO_PORT0, GPIO_PORT28,
-				GPIO_PORT35, GPIO_PORT141,
-				GPIO_PORT202},
-	.portcr		= {PORTCR0, PORTCR28,
-					PORTCR35, PORTCR141,
-					PORTCR202},
-	.get_portcr_value = tps80032_get_portcr_value,
-	.set_portcr_value = tps80032_set_portcr_value,
-	.num_subdevs	= ARRAY_SIZE(tps80032_devs),
-	.subdevs	= tps80032_devs,
-	.irq_base	= ENT_TPS80032_IRQ_BASE,
-};
 
 static struct i2c_board_info __initdata i2c0_devices[] = {
 #ifdef CONFIG_PMIC_INTERFACE
