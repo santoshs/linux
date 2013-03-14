e64e98c239d3d9a68e4b441495d12a9ba335ca37
David OLIVIER <ext-david.olivier@renesasmobile.com>
2013-03-14 15:17:24 +0100

    PN13031157625 : correct warning in l2mux.h and l3mhdp.c

--- net/mhi/l3mhdp.c
+++ net/mhi/l3mhdp.c
@@ -343,7 +343,7 @@ mhdp_netdev_ioctl(struct net_device *dev, struct ifreq *ifr, int cmd)
 
 			tunnel->free_pdn = 0;
 
-			strcpy(&k_parms.name, tunnel->dev->name);
+			strcpy(k_parms.name, tunnel->dev->name);
 
 			if (copy_to_user(u_parms, &k_parms,
 					sizeof(struct mhdp_tunnel_parm)))
