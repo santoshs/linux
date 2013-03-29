54442019203851faacb72a4f694d8e87439458cf
Sudharma Damgatla <Sudharma.Damgatla@renesasmobile.com>
2013-03-29 16:17:59 +0530

    fix for MK13032032059 : Wi-Fi deprecated code removal

--- drivers/net/wireless/bcmdhd/dhd_sec_feature.h
+++ drivers/net/wireless/bcmdhd/dhd_sec_feature.h
@@ -77,7 +77,13 @@
 #undef USE_CID_CHECK
 #endif
 
-#if defined(CONFIG_MACH_U2EVM)
+#if defined(CONFIG_MACH_GARDALTE)
+#define READ_MACADDR
+#undef HW_OOB
+#undef USE_CID_CHECK
+#endif
+
+#if defined(CONFIG_MACH_LT02LTE)
 #define READ_MACADDR
 #undef HW_OOB
 #undef USE_CID_CHECK
