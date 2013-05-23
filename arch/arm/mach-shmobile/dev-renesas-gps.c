e651de243b2dabcf132d55056f6bf3d4f9f495d0
Shinto John <johshint@babld012.rmad.renesasmobile.com>
2013-05-23 12:26:22 +0530

    NRST code removal as BCM chip dont use it

--- arch/arm/mach-shmobile/dev-renesas-gps.c
+++ arch/arm/mach-shmobile/dev-renesas-gps.c
@@ -1,55 +1,41 @@
-#include <mach/board-u2evm-renesas-gps.h>
-#include <mach/r8a73734.h>
+/*
+ * GPS GPIO control
+ *
+ *  Copyright (C) 2012 Samsung, Inc.
+ *  Copyright (C) 2012 Google, Inc.
+ *
+ *  This program is free software; you can redistribute it and/or modify
+ *  it under the terms of the GNU General Public License as published by
+ *  the Free Software Foundation; either version 2 of the License, or
+ *  (at your option) any later version.
+ *
+ *  This program is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *  GNU General Public License for more details.
+ *
+ *  You should have received a copy of the GNU General Public License
+ *  along with this program; if not, write to the Free Software
+ *  Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
+ *
+ */
+#include <mach/dev-gps.h>
+#include <linux/kernel.h>
+#include <linux/init.h>
+#include <linux/module.h>
+#include <linux/platform_device.h>
+#include <linux/delay.h>
+#include <linux/err.h>
+#include <linux/gpio.h>
+#include <mach/r8a7373.h>
 #include <mach/gpio.h>
-#ifdef CONFIG_PMIC_INTERFACE
-#include <linux/pmic/pmic-tps80032.h>
-#include <linux/pmic/pmic.h>
-#endif
-#include <linux/d2153/core.h>
+#include <mach/board.h>
 
-
+#include <linux/sysfs.h>
+#include <linux/proc_fs.h>
 struct class *gps_class;
 
-static ssize_t GNSS_NRST_value_show(struct device *dev,
-                                    struct device_attribute *attr, char *buf)
-{
-        int value = gpio_get_value(GPIO_PORT10);
-
-        return sprintf(buf, "%d\n", value);
-}
-
-static ssize_t GNSS_NRST_value_store(struct device *dev,
-                                     struct device_attribute *attr,
-                                     const char *buf, size_t count)
-{
-        long value;
-        int  ret;
-
-        ret = strict_strtol(buf, 0, &value);
-        if (ret < 0)
-                return ret;
-
-#ifdef CONFIG_MFD_D2153
-                if (1 == value)
-                        d2153_clk32k_enable(1);         /* on */
-                else
-                        d2153_clk32k_enable(0);         /* off */
-#endif /* CONFIG_MFD_D2153 */
-#if defined(CONFIG_PMIC_INTERFACE)
-                if (1 == value)
-                        pmic_clk32k_enable(CLK32KG, TPS80032_STATE_ON);
-                else
-                        pmic_clk32k_enable(CLK32KG, TPS80032_STATE_OFF);
-#endif /* CONFIG_PMIC_INTERFACE */
-
-        printk(KERN_ALERT "%s: %d\n", __func__, value);
-
-        gpio_set_value(GPIO_PORT10, value);
-
-        return count;
-}
-
-DEVICE_ATTR(value_nrst, 0644, GNSS_NRST_value_show, GNSS_NRST_value_store);
+#define FUNC2_MODE_SCIFB 0x02
 
 static ssize_t GNSS_EN_value_show(struct device *dev,
                                 struct device_attribute *attr, char *buf)
@@ -70,7 +56,12 @@ static ssize_t GNSS_EN_value_store(struct device *dev,
         if (ret < 0)
                 return ret;
 
-        printk(KERN_ALERT "%s: %d\n", __func__, value);
+        if(value != 0)
+	  gpio_pull_up_port(GPIO_PORT78);
+        else
+	  gpio_pull_off_port(GPIO_PORT78);
+
+        printk(KERN_ALERT "%s: %ld\n", __func__, value);
 
         gpio_set_value(GPIO_PORT11, value);
 
@@ -78,15 +69,6 @@ static ssize_t GNSS_EN_value_store(struct device *dev,
 }
 
 DEVICE_ATTR(value_en, 0644, GNSS_EN_value_show, GNSS_EN_value_store);
-
-static const struct attribute *GNSS_NRST_attrs[] = {
-        &dev_attr_value_nrst.attr,
-        NULL,
-};
-
-static const struct attribute_group GNSS_NRST_attr_group = {
-        .attrs = (struct attribute **) GNSS_NRST_attrs,
-};
 
 static const struct attribute *GNSS_EN_attrs[] = {
         &dev_attr_value_en.attr,
@@ -97,61 +79,53 @@ static const struct attribute_group GNSS_EN_attr_group = {
         .attrs = (struct attribute **) GNSS_EN_attrs,
 };
 
+
 void gps_gpio_init(void)
 {
-        struct device *gps_dev;
-        struct device *gnss_nrst_dev;
-        struct device *gnss_en_dev;
-        int    status = -EINVAL;
+	struct device *gps_dev;
 
-        gps_class = class_create(THIS_MODULE, "gps");
-        if (IS_ERR(gps_class)) {
-                pr_err("Failed to create class(sec)!\n");
-                return PTR_ERR(gps_class);
-        }
-        BUG_ON(!gps_class);
+	gps_class = class_create(THIS_MODULE, "gps");
+	if (IS_ERR(gps_class)) {
+		pr_err("Failed to create class(sec)!\n");
+		return;
+	}
+	BUG_ON(!gps_class);
 
-        gps_dev = device_create(gps_class, NULL, 0, NULL, "device_gps");
-        BUG_ON(!gps_dev);
+	gps_dev	= device_create(gps_class, NULL, 0,	NULL, "bcm4752");
+	BUG_ON(!gps_dev);
 
-        gnss_nrst_dev = device_create(gps_class, gps_dev, 0, NULL, "GNSS_NRST");
-        BUG_ON(!gnss_nrst_dev);
+	printk("gps_gpio_init!!");
 
-        gnss_en_dev = device_create(gps_class, gps_dev, 0, NULL, "GNSS_EN");
-        BUG_ON(!gnss_en_dev);
+	/* SCIFB1::UART mode & Function mode settings. */
+	gpio_request(GPIO_FN_SCIFB1_RXD, NULL);
+	gpio_pull_up_port(GPIO_PORT79);
 
-        status = sysfs_create_group(&gnss_nrst_dev->kobj,
-                                    &GNSS_NRST_attr_group);
+	gpio_request(GPIO_FN_SCIFB1_TXD, NULL);
+	gpio_pull_off_port(GPIO_PORT78);
 
-        if (status)
-                pr_debug("%s: status for sysfs_create_group %d\n",
-                         __func__, status);
+	gpio_request(GPIO_FN_SCIFB1_CTS, NULL);
+	gpio_pull_off_port(GPIO_PORT77);
 
-        status = sysfs_create_group(&gnss_en_dev->kobj, &GNSS_EN_attr_group);
+	gpio_request(GPIO_FN_SCIFB1_RTS, NULL);
+	gpio_pull_off_port(GPIO_PORT76);
 
-        if (status)
-                pr_debug("%s: status for sysfs_create_group %d\n",
-                         __func__, status);
 
-        printk(KERN_ALERT "gps_gpio_init!!");
+	/* GPS Settings */
+	gpio_request(GPIO_PORT11, "GPS_PWR_EN");
+	gpio_pull_off_port(GPIO_PORT11);
+	gpio_direction_output(GPIO_PORT11, 0);
 
-                gpio_request(GPIO_FN_SCIFB1_RXD, NULL);
-                gpio_pull_up_port(GPIO_PORT79);
-                gpio_request(GPIO_FN_SCIFB1_TXD, NULL);
-                gpio_pull_off_port(GPIO_PORT78);
-                gpio_request(GPIO_FN_SCIFB1_CTS, NULL);
-                gpio_pull_up_port(GPIO_PORT77);
-                gpio_request(GPIO_FN_SCIFB1_RTS, NULL);
-                gpio_pull_off_port(GPIO_PORT76);
+	gpio_request(GPIO_PORT48, "GPS_ECLK_26M");
+	gpio_pull_off_port(GPIO_PORT48);
+	gpio_direction_output(GPIO_PORT48, 0);
 
-                /* GPS Settings */
-                gpio_request(GPIO_PORT10, "GNSS_NRST");
-                gpio_pull_off_port(GPIO_PORT10);
-                gpio_direction_output(GPIO_PORT10, 0);
+	gpio_export(GPIO_PORT11, 1);
+	gpio_export(GPIO_PORT48, 1);
 
-                gpio_request(GPIO_PORT11, "GNSS_EN");
-                gpio_pull_off_port(GPIO_PORT11);
-                gpio_direction_output(GPIO_PORT11, 0);
+	gpio_export_link(gps_dev, "GPS_PWR_EN", GPIO_PORT11);
+	gpio_export_link(gps_dev, "GPS_ECLK_26M", GPIO_PORT48);
 
-        printk("gps_gpio_init done!!\n");
+	printk("gps_gpio_init done!!");
 }
+
+
