b4295ec1abe486b9ec6c3a7b43facbd9e3dfceaf
Kevin Bracey <kevin.bracey@renesasmobile.com>
2013-07-05 16:20:05 +0300

    Revert sound changes in MG1
    Sound changes in MG1 were a fix for SSGLOGEO2-1207, but we've since
    withdrawn those changes due to side effects (and we also reverted the
    earlier changes in MF9, so they don't compile anyway). Remove them from
    this merge path.
    
    Suggested-by: Hiromasa Nakayama <hiromasa.nakayama@renesasmobile.com>

--- sound/soc/sh/fsi-d2153.c
+++ sound/soc/sh/fsi-d2153.c
@@ -86,8 +86,6 @@ static void fsi_d2153_set_active(struct snd_soc_codec *codec,
 	struct snd_soc_dapm_widget *w;
 	struct fsi_d2153_priv *priv =
 			snd_soc_card_get_drvdata(codec->card);
-	static int _d2153_set_dac_active = false;
-	static int _d2153_set_adc_active = false;
 
 	if (strstr(stream, D2153_PLAYBACK_STREAM_NAME))
 		w = playback_widget;
@@ -115,9 +113,6 @@ static void fsi_d2153_set_active(struct snd_soc_codec *codec,
 		w->name, w->active);
 	snd_soc_dapm_sync(&codec->dapm);
 
-	if((!_d2153_set_dac_active) && (!_d2153_set_adc_active))
-		d2153_set_aif_adjust(codec);
-
 	/* Un-mute output device */
 	if (playback_widget == w) {
 		if (active) {
@@ -142,7 +137,6 @@ static void fsi_d2153_set_active(struct snd_soc_codec *codec,
 				msleep(50);
 			}
 		}
-		_d2153_set_dac_active = active;
 	} else {
 		if (active) {
 			sndp_log_info("adc unmute\n");
@@ -152,7 +146,6 @@ static void fsi_d2153_set_active(struct snd_soc_codec *codec,
 				D2153_ADC_MUTE_EN, 0);
 		}
 		msleep(20);
-		_d2153_set_adc_active = active;
 	}
 }
 
