62f892dfd4e0462cfd825d5e13ef5cfe787be0b8
Kari Suvanto <kari.suvanto@renesasmobile.com>
2013-09-28 16:40:52 +0300

    remove board names from include guards in common headers
    (cherry picked from commit 392785b52ef374f21ba3323b878c9a0ec2d296bc)
    Signed-off-by: Kari Suvanto <kari.suvanto@renesasmobile.com>
    
    Conflicts:
    	arch/arm/mach-shmobile/include/mach/dev-sensor.h

--- arch/arm/mach-shmobile/include/mach/dev-sensor.h
+++ arch/arm/mach-shmobile/include/mach/dev-sensor.h
@@ -1,5 +1,5 @@
-#ifndef __ASM_MACH_BOARD_GARDALTE_SENSOR_H
-#define __ASM_MACH_BOARD_GARDALTE_SENSOR_H
+#ifndef __DEV_SENSOR_H
+#define __DEV_SENSOR_H
 
 extern void __init board_sensor_init(void);
 
@@ -7,4 +7,4 @@ extern void __init amethyst_board_sensor_init(void);
 
 void __init amethyst_board_sensor_init(void);
 
-#endif // __ASM_MACH_BOARD_GARDALTE_SENSOR_H
+#endif // __DEV_SENSOR_H
