61cc43da63609492ec49f18fbd19634dc9bb43b3
Ram <rambik@broadcom.com>
2013-10-26 14:51:11 +0530

    BT: Bluetooth initial implementation

---
+++ arch/arm/mach-shmobile/include/mach/dev-bt-lpm.h
@@ -0,0 +1,6 @@
+#ifndef __ASM_DEV_BT_LPM_H
+#define __ASM_DEV_BT_LPM_H
+
+extern struct platform_device board_bcmbt_lpm_device;
+
+#endif // __ASM_DEV_BT_LPM_H
--- include/linux/tty.h
+++ include/linux/tty.h
@@ -35,6 +35,7 @@
 #define N_TRACESINK	23	/* Trace data routing for MIPI P1149.7 */
 #define N_TRACEROUTER	24	/* Trace data routing for MIPI P1149.7 */
 #define N_PHONET        25      /* PHONET over USB/ACM */
+#define N_BRCM_HCI      26      /* Broadcom Bluetooth HCI */
 
 #ifdef __KERNEL__
 #include <linux/fs.h>
