aa2e9abb5cdd4ab49a595ca8d2d17a9dabe888a4
Asheefikbal <ext-Asheefikbal.Kandagall@renesasmobile.com>
2013-11-11 16:27:45 +0530

    Reverted SHDMA driver to 3.4 in 3.10 kernel
    This patch has the SHDMA driver from 3.4.5 kernel. Once we confirm that
    the crashes/instabilities, that arise because of SHDMA gets vanished
    we can decide to merge missing features or retain the 3.4 itself
    
    Signed-off-by: Asheef Ikbal <asheefikbal.kandagall@broadcom.com>

--- drivers/dma/sh/shdma.h
+++ drivers/dma/sh/shdma.h
@@ -13,83 +13,70 @@
 #ifndef __DMA_SHDMA_H
 #define __DMA_SHDMA_H
 
-#include <linux/sh_dma.h>
-#include <linux/shdma-base.h>
+#include <linux/clk.h>
+#include <linux/err.h>
 #include <linux/dmaengine.h>
 #include <linux/interrupt.h>
 #include <linux/list.h>
-#include <linux/dma-direction.h>
 
-/* DMA descriptor control */
-enum shdma_desc_status {
-	DESC_IDLE,
-	DESC_PREPARED,
-	DESC_SUBMITTED,
-	DESC_COMPLETED, /* completed, have to call callback */
-	DESC_WAITING,   /* callback called, waiting for ack / re-submit */
-};
-
-#define NR_DESCS_PER_CHANNEL 128
-
-#define to_shdma_chan(c) container_of(c, struct shdma_chan, dma_chan)
-#define to_shdma_dev(d) container_of(d, struct shdma_dev, dma_dev)
-
-#define SH_DMAE_MAX_CHANNELS 20
-#define SH_DMAE_TCR_MAX 0x00FFFFFF	/* 16MB */
+#define SH_DMAC_MAX_CHANNELS 20
+#define SH_DMA_SLAVE_NUMBER 256
+#define SH_DMA_TCR_MAX 0x00FFFFFF	/* 16MB */
 
 struct device;
 
+struct sh_dmae_desc_mem {
+	u32 sar;
+	u32 dar;
+	u32 tcr;
+	u32 rsvd;
+};
+
 struct sh_dmae_chan {
-	struct shdma_chan shdma_chan;
-	struct sh_dmae_slave_config *config; /* Slave DMA configuration */
-	int xmit_shift;			/* log_2(bytes_per_xfer) */
+	spinlock_t desc_lock;           /* Descriptor operation lock */
+	struct list_head ld_queue;      /* Link descriptors queue */
+	struct list_head ld_free;       /* Link descriptors free */
+	struct dma_chan common;         /* DMA common channel */
+	struct device *dev;             /* Channel device */
+	struct tasklet_struct tasklet;  /* Tasklet */
+	int addr_error;                 /* Adress error interrupt has been
+					 * raised */
+	int descs_allocated;            /* desc count */
+	int xmit_shift;                 /* log_2(bytes_per_xfer) */
+	int irq;
+	int id;				/* Raw id of this channel */
 	u32 __iomem *base;
-	char dev_id[16];		/* unique name per DMAC of channel */
-	int pm_error;
+	char dev_id[16];                /* unique name per DMAC of channel */
 	void __iomem *desc_mem;
 	phys_addr_t desc_pmem;
 	int desc_mode;
 	int no_of_descs;
-	enum dma_transfer_direction direction;
+	u32 chcr;
 	struct scatterlist *sgl;
 	unsigned int sg_len;
 	dma_addr_t addr;
+	enum dma_transfer_direction direction;
 };
 
 struct sh_dmae_device {
-	struct shdma_dev shdma_dev;
-	struct sh_dmae_chan *chan[SH_DMAE_MAX_CHANNELS];
+	struct dma_device common;
+	struct sh_dmae_chan *chan[SH_DMAC_MAX_CHANNELS];
 	struct sh_dmae_pdata *pdata;
 	struct list_head node;
 	u32 __iomem *chan_reg;
 	u16 __iomem *dmars;
-	void __iomem *desc_mem;
-	phys_addr_t desc_pmem;
 	unsigned int chcr_offset;
 	u32 chcr_ie_bit;
+	void __iomem *desc_mem;
+	phys_addr_t desc_pmem;
+
+	struct clk *clk;
 };
 
-
-struct sh_dmae_desc {
-	struct sh_dmae_regs hw;
-	struct shdma_desc shdma_desc;
-};
-
-#define to_sh_chan(chan) container_of(chan, struct sh_dmae_chan, shdma_chan)
+#define to_sh_chan(chan) container_of(chan, struct sh_dmae_chan, common)
 #define to_sh_desc(lh) container_of(lh, struct sh_desc, node)
 #define tx_to_sh_desc(tx) container_of(tx, struct sh_desc, async_tx)
-#define to_sh_dev(chan) container_of(chan->shdma_chan.dma_chan.device,\
-				     struct sh_dmae_device, shdma_dev.dma_dev)
-/* TODO - ### Check, is this the right place ?? */
-void dmae_rpt_halt(struct shdma_chan *schan);
-int dmae_rpt_init_reg(struct shdma_chan *schan);
-void dmae_rpt_start(struct shdma_chan *schan);
-
-struct dma_async_tx_descriptor *sh_dmae_rpt_prep_sg(
-	struct shdma_chan *schan,
-	struct scatterlist *sgl, unsigned int sg_len, dma_addr_t *addr,
-	enum dma_data_direction direction, unsigned long flags);
-void set_desc_mode(struct shdma_chan *schan, bool val);
-int is_desc_mode(struct shdma_chan *schan);
+#define to_sh_dev(chan) container_of(chan->common.device,\
+				     struct sh_dmae_device, common)
 
 #endif	/* __DMA_SHDMA_H */
