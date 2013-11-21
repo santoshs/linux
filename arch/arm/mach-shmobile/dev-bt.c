a2b8472b9a3e03e735c1014e99766b556c58ef61
Kevin Bracey <kbracey@broadcom.com>
2013-11-21 13:29:33 +0200

    dev-bt: Refine device registration
    Compact the shared BT device registration functions by using
    platform_device_register_data. We can efficiently just create automatic
    data with the passed parameters, which is then memdup'ed by the core.
    
    Thus overhead is minimised if the functions are not called because the
    current board is using DT - we can conceptually support both DT and
    non-DT boards in one image.
    
    Signed-off-by: Kevin Bracey <kbracey@broadcom.com>

--- arch/arm/mach-shmobile/dev-bt.c
+++ arch/arm/mach-shmobile/dev-bt.c
@@ -7,77 +7,42 @@
 #include <mach/dev-bt.h>
 #include <mach/r8a7373.h>
 
-static struct bcm_bt_rfkill_platform_data bcm_bt_rfkill_data = {
-	.bcm_bt_rfkill_vreg_gpio = -1,
-	.bcm_bt_rfkill_n_reset_gpio = -1,
-};
-
-static struct platform_device board_bcmbt_rfkill_device = {
-	.name = "bcm-bt-rfkill",
-	.id   = -1,
-	.dev  = {
-		.platform_data = &bcm_bt_rfkill_data,
-	},
-};
-
 void __init add_bcmbt_rfkill_device(int vreg_gpio, int n_reset_gpio)
 {
-	int ret;
-	bcm_bt_rfkill_data.bcm_bt_rfkill_vreg_gpio = vreg_gpio;
-	bcm_bt_rfkill_data.bcm_bt_rfkill_n_reset_gpio = n_reset_gpio;
-	ret = platform_device_register(&board_bcmbt_rfkill_device);
+	struct bcm_bt_rfkill_platform_data data = {
+		.bcm_bt_rfkill_vreg_gpio = vreg_gpio,
+		.bcm_bt_rfkill_n_reset_gpio = n_reset_gpio,
+	};
+	int ret = platform_device_register_data(NULL, "bcm-bt-rfkill", -1,
+						&data, sizeof data);
 	if (ret)
-		pr_err("%s failed to add bcm_bt_rfkill_data %d",
-				__func__, ret);
+		pr_err("failed to register bcm-bt-rfkill device %d", ret);
 }
 
 #ifdef CONFIG_BCM_BZHW
-static struct bcm_bzhw_platform_data bcm_bzhw_data = {
-	.gpio_bt_wake   = -1,
-	.gpio_host_wake = -1,
-};
-
-static struct platform_device board_bcm_bzhw_device = {
-	.name = "bcm_bzhw",
-	.id = -1,
-	.dev = {
-		.platform_data = &bcm_bzhw_data,
-	},
-};
-
 void __init add_bcm_bzhw_device(int gpio_bt_wake, int gpio_host_wake)
 {
-	bcm_bzhw_data.bt_wake_gpio = gpio_bt_wake;
-	bcm_bzhw_data.host_wake_gpio = gpio_host_wake;
-	int ret = platform_device_register(&board_bcmbt_lpm_device);
+	struct bcm_bzhw_platform_data data = {
+		.gpio_bt_wake   = gpio_bt_wake,
+		.gpio_host_wake = gpio_host_wake,
+	};
+	int ret = platform_device_register_data(NULL, "bcm_bzhw", -1,
+						&data, sizeof data);
 	if (ret)
-		pr_err("%s failed to add board_bcm_bzhw_device %d",
-				__func__, ret);
+		pr_err("failed to register bcm_bzhw device %d", ret);
 }
 
 #endif
 
 
-static struct bcm_bt_lpm_platform_data brcm_bt_lpm_data = {
-	.bt_wake_gpio = -1,
-	.host_wake_gpio = -1,
-};
-
-static struct platform_device board_bcmbt_lpm_device = {
-	.name = "bcm-bt-lpm",
-	.id   = -1,
-	.dev  = {
-		.platform_data = &brcm_bt_lpm_data,
-	},
-};
-
 void __init add_bcmbt_lpm_device(int gpio_bt_wake, int gpio_host_wake)
 {
-	int ret;
-	brcm_bt_lpm_data.bt_wake_gpio = gpio_bt_wake;
-	brcm_bt_lpm_data.host_wake_gpio = gpio_host_wake;
-	ret = platform_device_register(&board_bcmbt_lpm_device);
+	struct bcm_bt_lpm_platform_data data = {
+		.bt_wake_gpio = gpio_bt_wake,
+		.host_wake_gpio = gpio_host_wake,
+	};
+	int ret = platform_device_register_data(NULL, "bcm-bt-lpm", -1,
+						&data, sizeof data);
 	if (ret)
-		pr_err("%s failed to add board_bcmbt_lpm_device %d",
-				__func__, ret);
+		pr_err("failed to register bcm-bt-lpm device %d", ret);
 }
